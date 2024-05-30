813044b9b3b72f9d046fc901216accf4ca90d508
rinnegard <victor.rinnegard@hotmail.com>
2024-05-30 10:47:28 +0200

    fixed css positioning and added event for keydown

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 console.log("Loaded");
+
+const tiles = document.querySelectorAll(".tile");
+console.log(tiles);
+
+window.addEventListener("keydown", (event) => {
+    switch (event.key) {
+        case "ArrowRight":
+        case "d":
+            moveTiles();
+            break;
+        case "ArrowLeft":
+        case "a":
+            moveTiles();
+            break;
+        case "ArrowDown":
+        case "s":
+            moveTiles();
+            break;
+        case "ArrowUp":
+        case "w":
+            moveTiles();
+            break;
+        default:
+            break;
+    }
+})
+
+function moveTiles(direction) {
+    tiles.forEach((tile) => {
+        tile.style.top = "200px";
+    })
+}
